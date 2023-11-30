3f9a01023c4ba9a92b55db09cd2081e91fadeecb
Your Name <you@example.com>
2023-11-30 15:40:04 -0600

    added test for helper functions

--- crudapp/app/tests.py
+++ crudapp/app/tests.py
@@ -1,15 +1,15 @@
 from django.test import TestCase
-
-# Create your tests here.
-
 from django.test import TestCase
 from .models import Item
+from app import models
+
+# Create your tests here.
 
 
 # Create your tests here.
 class TestItem_test_cases(TestCase):
     def test_item_creation(self):
-        item1 = Item(title="Vase")
+        item1 = models.make_item("Vase")
         item1.save()
         self.assertEqual(Item.objects.count(), 1)
 
@@ -17,7 +17,7 @@ class TestItem_test_cases(TestCase):
         Item1 = Item.objects.create(title="Vase")
         Item1.save()
 
-        items = Item.objects.all()
+        items = models.read_all_items()
         title = [items.title for items in items]
         self.assertEqual(len(title), 1)
 
@@ -25,14 +25,14 @@ class TestItem_test_cases(TestCase):
         D = Item(title="Vase")
         D.save()
 
-        D.delete()
+        models.delete_item("Vase")
         items = Item.objects.all()
         self.assertEqual(len(items), 0)
 
     def test_update_item(self):
         Item1 = Item.objects.create(title="Vase")
         Item1.save()
-        Item1.title = "Vase: Blue"
+        models.update_item(Item.title, "Vase: Blue")
         Item1.save()
 
         self.assertEqual(Item1.title, "Vase: Blue")
@@ -40,11 +40,11 @@ class TestItem_test_cases(TestCase):
     def test_read_by_title(self):
         Item1 = Item.objects.create(title="Vase")
         Item1.save()
-        Item_return = Item.objects.get(title="Vase")
+        Item_return = models.read_item_by_title("Vase")
         self.assertEqual(Item_return.title, "Vase")
 
     def test_filtered_search(self):
         Item1 = Item.objects.create(title="Vase")
         Item1.save()
-        Item_return = Item.objects.filter(title="Vase")
+        Item_return = models.item_filter("Vase")
         self.assertEqual(Item_return[0].title, "Vase")
