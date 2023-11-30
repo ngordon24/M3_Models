ad0082862ec2d2ed3484a8ebc011df381443c7bb
Your Name <you@example.com>
2023-11-30 13:17:39 -0600

    fixed failing test

--- crudapp/app/models.py
+++ crudapp/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    Item = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
 
     def __str__(self):
         return self.title
--- crudapp/app/tests.py
+++ crudapp/app/tests.py
@@ -7,15 +7,15 @@ from .models import Item
 
 
 # Create your tests here.
-class TestGame_test_cases(TestCase):
+class TestItem_test_cases(TestCase):
     def test_item_creation(self):
-        item = item(title="Vase")
-        item.save()
-        self.assertEqual(item.objects.count(), 1)
+        item1 = Item(title="Vase")
+        item1.save()
+        self.assertEqual(Item.objects.count(), 1)
 
     def test_read_all_items(self):
-        Item = Item.objects.create(title="Vase")
-        Item.save()
+        Item1 = Item.objects.create(title="Vase")
+        Item1.save()
 
         items = Item.objects.all()
         title = [items.title for items in items]
@@ -29,22 +29,22 @@ class TestGame_test_cases(TestCase):
         items = Item.objects.all()
         self.assertEqual(len(items), 0)
 
-    def test_update_game(self):
-        Item = Item.objects.create(title="Vase")
-        Item.save()
-        Item.title = "Vase: Blue"
-        Item.save()
+    def test_update_item(self):
+        Item1 = Item.objects.create(title="Vase")
+        Item1.save()
+        Item1.title = "Vase: Blue"
+        Item1.save()
 
-        self.assertEqual(Item.title, "Vase: Black")
+        self.assertEqual(Item1.title, "Vase: Blue")
 
     def test_read_by_title(self):
-        Item = Item.objects.create(title="Vase")
-        Item.save()
+        Item1 = Item.objects.create(title="Vase")
+        Item1.save()
         Item_return = Item.objects.get(title="Vase")
         self.assertEqual(Item_return.title, "Vase")
 
     def test_filtered_search(self):
-        Item = Item.objects.create(title="Vase")
-        Item.save()
+        Item1 = Item.objects.create(title="Vase")
+        Item1.save()
         Item_return = Item.objects.filter(title="Vase")
         self.assertEqual(Item_return[0].title, "Vase")
